d939aefe59c0af182d8e83088ae390800d771b38
r1k0 <erick@openchill.org>
2013-02-17 23:22:45 +0000

    lilo now works

--- modules/bootloader_x86.sh
+++ modules/bootloader_x86.sh
@@ -79,19 +79,12 @@ configure_bootloader_grub2() {
 
 configure_bootloader_lilo() {
     debug configure_bootloader_lilo "configuring /etc/lilo.conf"
-#    echo -e "set default=0\nset timeout=5\n" > ${chroot_dir}/etc/lilo.conf
     local boot_root="$(get_boot_and_root)"
     local boot="$(echo ${boot_root} | cut -d '|' -f1)"
     local boot_device="$(get_device_and_partition_from_devnode ${boot} | cut -d '|' -f1)"
     local boot_minor="$(get_device_and_partition_from_devnode ${boot}  | cut -d '|' -f2)"
     local root="$(echo ${boot_root} | cut -d '|' -f2)"
     local kernel_initrd="$(get_kernel_and_initrd)"
-echo boot_root $boot_root
-echo boot $boot
-echo boot_device $boot_device
-echo boot_minor $boot_minor
-echo root $root
-echo kernel_initrd $kernel_initrd
     echo -e "boot=${boot_device}\n" > ${chroot_dir}/etc/lilo.conf
     echo -e "prompt\n" >> ${chroot_dir}/etc/lilo.conf
     echo -e "timeout=20\n" >> ${chroot_dir}/etc/lilo.conf
@@ -99,36 +92,18 @@ echo kernel_initrd $kernel_initrd
         local kernel="$(echo ${k} | cut -d '|' -f1)"
         local initrd="$(echo ${k} | cut -d '|' -f2)"
         local kv="$(echo ${kernel} | sed -e 's:^kernel-*-[^-]\+-::' | sed -e 's:[^-]\+-::')"
-echo kernel $kernel
-echo initrd $initrd
-echo kv $kv
-echo hostname $hostname
         echo -e "image=/boot/${kernel}\n"              >> ${chroot_dir}/etc/lilo.conf
         echo -e "  label=${hostname}\n"                >> ${chroot_dir}/etc/lilo.conf
         echo -e "  read-only\n"                        >> ${chroot_dir}/etc/lilo.conf
+        # FIXME do something about non initramfs setup?
 #        echo -e "  root=${root}\n"                     >> ${chroot_dir}/etc/lilo.conf # this is for non initramfs enabled
         echo -e "  append=\"root=/dev/ram0 init=/linuxrc ramdisk=8192 real_root=${root} ${bootloader_kernel_args}\"\n" >> ${chroot_dir}/etc/lilo.conf
         echo -e "  initrd=/boot/${initrd}\n\n"         >> ${chroot_dir}/etc/lilo.conf
-#        echo "menuentry \"${distro} Linux ${kv}\" {" >> ${chroot_dir}/etc/lilo.conf
-#        local grub_device="$(map_device_to_lilo_device ${boot_device})"
-#        if [ -z "${grub_device}" ]; then
-#            error "Could not map boot device ${boot_device} to lilo device"
-#            return 1
-#        fi
-#        echo -en "set root=(${grub_device},$(expr ${boot_minor}))\nlinux /${kernel} " >> ${chroot_dir}/etc/lilo.conf
-#        if [ -z "${initrd}" ]; then
-#            echo "root=${root}" >> ${chroot_dir}/etc/lilo.conf
-#        else
-#            echo "root=/dev/ram0 init=/linuxrc ramdisk=8192 real_root=${root} ${bootloader_kernel_args}" >> ${chroot_dir}/etc/lilo.conf
-#            echo -e "initrd /${initrd}\n" >> ${chroot_dir}/etc/lilo.conf
-#        fi
-#        echo -e "}\n" >> ${chroot_dir}/etc/lilo.conf
     done
     if ! spawn_chroot "grep -v rootfs /proc/mounts > /etc/mtab"; then
         error "Could not copy /proc/mounts to /etc/mtab"
         return 1
     fi
-    [ -z "${bootloader_install_device}" ] && bootloader_install_device="$(get_device_and_partition_from_devnode ${boot} | cut -d '|' -f1)"
     if ! spawn_chroot "lilo"; then
         error "Could not run lilo"
         return 1
